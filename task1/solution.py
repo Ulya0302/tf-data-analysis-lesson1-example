de923a5d0cf1616494a3fef2d2c3831e197ae19e
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-01-29 14:02:58 +0300

    Update and rename decision.py to solution.py

---
+++ task1/solution.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+# your variant
+# don't change variable name
+variant = 0
+
+def solution(x: np.array) -> float:
+    # place your solution here
+    # don't change function name
+    # and arguments of function
+    return x.mean() # your answer
